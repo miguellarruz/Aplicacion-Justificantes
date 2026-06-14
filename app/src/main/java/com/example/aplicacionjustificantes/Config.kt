5df70707422daa3425990167edb092dda074d96c
Fernando <2024310272@uteq.edu.mx>
2026-06-14 17:43:17 -0600

    cambio de mejora

--- app/src/main/java/com/example/aplicacionjustificantes/Config.kt
+++ app/src/main/java/com/example/aplicacionjustificantes/Config.kt
@@ -1,7 +1,7 @@
 package com.example.aplicacionjustificantes
 
 object Config {
-    // 🔑 CON http:// (Sin la S) para evitar el error de certificado inválido
+    // 🔑 Estricto con http:// para que use la configuración que acabamos de crear
     const val IP_SERVIDOR = "http://justificateq.atwebpages.com/justificantes_api/"
     const val USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 }
