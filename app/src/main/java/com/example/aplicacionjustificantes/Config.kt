38488cebcd418ce3611c50ef5523ee553f2470aa
Fernando <2024310272@uteq.edu.mx>
2026-06-14 15:48:28 -0600

    cambio de mejora

--- app/src/main/java/com/example/aplicacionjustificantes/Config.kt
+++ app/src/main/java/com/example/aplicacionjustificantes/Config.kt
@@ -1,5 +1,5 @@
 package com.example.aplicacionjustificantes
 
 object Config {
-    const val URL_BASE = "http://justificateq.xo.je/justificantes_api/"
+    const val IP_SERVIDOR = "http://justificateq.xo.je"
 }
