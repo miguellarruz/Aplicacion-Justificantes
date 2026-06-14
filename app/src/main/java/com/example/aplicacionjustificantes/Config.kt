2ea5d9eb7a6e48ce5a50704ee801f4969bc4889d
Fernando <2024310272@uteq.edu.mx>
2026-06-14 17:14:26 -0600

    cambio de mejora

--- app/src/main/java/com/example/aplicacionjustificantes/Config.kt
+++ app/src/main/java/com/example/aplicacionjustificantes/Config.kt
@@ -1,6 +1,7 @@
 package com.example.aplicacionjustificantes
 
 object Config {
-    const val IP_SERVIDOR = "http://justificateq.atwebpages.com/justificantes_api/"
+    // 🚀 CAMBIADO A HTTPS: Para obligar a la app a usar el tunel seguro y saltar restricciones del hosting
+    const val IP_SERVIDOR = "https://justificateq.atwebpages.com/justificantes_api/"
     const val USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 }
