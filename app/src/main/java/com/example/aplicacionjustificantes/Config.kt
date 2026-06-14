dabb2e061642f160bb54a90b8e094076f774cf4a
Fernando <2024310272@uteq.edu.mx>
2026-06-14 15:48:44 -0600

    cambio de mejora

--- app/src/main/java/com/example/aplicacionjustificantes/Config.kt
+++ app/src/main/java/com/example/aplicacionjustificantes/Config.kt
@@ -1,5 +1,5 @@
 package com.example.aplicacionjustificantes
 
 object Config {
-    const val IP_SERVIDOR = "http://justificateq.xo.je"
+    const val IP_SERVIDOR = "http://justificateq.xo.je/justificantes_api/"
 }
